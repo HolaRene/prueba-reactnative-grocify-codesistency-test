ed2f7a28267234a52eef75feaf337ffc3a7794f0
Jaime <176935470+HolaRene@users.noreply.github.com>
2026-03-22 21:19:38 -0600

    Los estilos sirven css

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,8 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Edita src/app/index.tsx Es la pagina principal.</Text>
-
+      <Text className="text-2xl font-bold">Donjoe.</Text>
+      <Text className="text-5xl font-bold text-neutral-400">Donjoe.</Text>
     </View>
   );
 }
